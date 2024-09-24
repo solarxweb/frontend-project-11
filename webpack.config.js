24f2ef5d1135c094707ce16e02fe04d9160fb972
dev <legendaryje@gmail.com>
2024-09-24 23:04:47 +0300

    try to fix workflow warning

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ export default {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
     clean: true,
   },
   devServer: {
